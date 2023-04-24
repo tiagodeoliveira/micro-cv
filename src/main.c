5b05be11ad7ed6b2bb65c1484793cde0f1cbeab6
Tiago Oliveira <tiagode@amazon.com>
2023-04-24 18:22:36 -0500

    .

--- src/main.c
+++ src/main.c
@@ -23,87 +23,155 @@
 #define WIFI_FAIL_BIT BIT1
 #define PART_BOUNDARY "123456789000000000000987654321"
 
+#define CAMERA_PIN_PWDN 32
+#define CAMERA_PIN_RESET -1
+#define CAMERA_PIN_XCLK 0
+#define CAMERA_PIN_SIOD 26
+#define CAMERA_PIN_SIOC 27
+#define CAMERA_PIN_D7 35
+#define CAMERA_PIN_D6 34
+#define CAMERA_PIN_D5 39
+#define CAMERA_PIN_D4 36
+#define CAMERA_PIN_D3 21
+#define CAMERA_PIN_D2 19
+#define CAMERA_PIN_D1 18
+#define CAMERA_PIN_D0 5
+#define CAMERA_PIN_VSYNC 25
+#define CAMERA_PIN_HREF 23
+#define CAMERA_PIN_PCLK 22
+#define XCLK_FREQ_HZ 20000000
+
 static const char *TAG = "INIT";
 
 static const char *_STREAM_CONTENT_TYPE =
     "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_STREAM_PART =
+    "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
 
 static httpd_handle_t server = NULL;
 static EventGroupHandle_t wifi_event_group;
 
-esp_err_t get_handler(httpd_req_t *req) {
-  const char resp[] = "URI GET Response";
-  httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+int app_camera_init() {
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = CAMERA_PIN_D0;
+  config.pin_d1 = CAMERA_PIN_D1;
+  config.pin_d2 = CAMERA_PIN_D2;
+  config.pin_d3 = CAMERA_PIN_D3;
+  config.pin_d4 = CAMERA_PIN_D4;
+  config.pin_d5 = CAMERA_PIN_D5;
+  config.pin_d6 = CAMERA_PIN_D6;
+  config.pin_d7 = CAMERA_PIN_D7;
+  config.pin_xclk = CAMERA_PIN_XCLK;
+  config.pin_pclk = CAMERA_PIN_PCLK;
+  config.pin_vsync = CAMERA_PIN_VSYNC;
+  config.pin_href = CAMERA_PIN_HREF;
+  config.pin_sccb_sda = CAMERA_PIN_SIOD;
+  config.pin_sccb_scl = CAMERA_PIN_SIOC;
+  config.pin_pwdn = CAMERA_PIN_PWDN;
+  config.pin_reset = CAMERA_PIN_RESET;
+  config.xclk_freq_hz = XCLK_FREQ_HZ;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+#ifdef CONFIG_ESP32_SPIRAM_SUPPORT
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.jpeg_quality = 25;
+  config.fb_count = 2;
+#else
+  config.frame_size = FRAMESIZE_SVGA;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+#endif
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    ESP_LOGE(TAG, "Camera init failed with error 0x%x", err);
+    return err;
+  }
   return ESP_OK;
 }
 
-// static esp_err_t stream_handler(httpd_req_t *req) {
-//   esp_err_t res = ESP_OK;
-//   res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
-//   if (res != ESP_OK) {
-//     return res;
-//   }
+static esp_err_t stream_handler(httpd_req_t *req) {
+  camera_fb_t *fb = NULL;
+  esp_err_t res = ESP_OK;
+  size_t _jpg_buf_len = 0;
+  uint8_t *_jpg_buf = NULL;
+  char *part_buf[64];
 
-//   while (true) {
-//     fb = esp_camera_fb_get();
-//     if (!fb) {
-//       Serial.println("Camera capture failed");
-//       res = ESP_FAIL;
-//     } else {
-//       if (fb->width > 400) {
-//         if (fb->format != PIXFORMAT_JPEG) {
-//           bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
-//           esp_camera_fb_return(fb);
-//           fb = NULL;
-//           if (!jpeg_converted) {
-//             Serial.println("JPEG compression failed");
-//             res = ESP_FAIL;
-//           }
-//         } else {
-//           _jpg_buf_len = fb->len;
-//           _jpg_buf = fb->buf;
-//         }
-//       }
-//     }
-//     if (res == ESP_OK) {
-//       size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART,
-//       _jpg_buf_len); res = httpd_resp_send_chunk(req, (const char *)part_buf,
-//       hlen);
-//     }
-//     if (res == ESP_OK) {
-//       res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
-//     }
-//     if (res == ESP_OK) {
-//       res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY,
-//                                   strlen(_STREAM_BOUNDARY));
-//     }
-//     if (fb) {
-//       esp_camera_fb_return(fb);
-//       fb = NULL;
-//       _jpg_buf = NULL;
-//     } else if (_jpg_buf) {
-//       free(_jpg_buf);
-//       _jpg_buf = NULL;
-//     }
-//     if (res != ESP_OK) {
-//       break;
-//     }
-//   }
-//   return res;
-// }
+  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+  if (res != ESP_OK) {
+    return res;
+  }
 
-httpd_uri_t uri_get = {
-    .uri = "/", .method = HTTP_GET, .handler = get_handler, .user_ctx = NULL};
+  while (true) {
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      ESP_LOGE(TAG, "Image capture failed");
+      res = ESP_FAIL;
+    } else {
+      if (fb->width > 400) {
+        if (fb->format != PIXFORMAT_JPEG) {
+          bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+          esp_camera_fb_return(fb);
+          fb = NULL;
+          if (!jpeg_converted) {
+            ESP_LOGE(TAG, "Image compression failed");
+            res = ESP_FAIL;
+          }
+        } else {
+          _jpg_buf_len = fb->len;
+          _jpg_buf = fb->buf;
+        }
+      }
+    }
+    if (res == ESP_OK) {
+      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+    }
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+    }
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY,
+                                  strlen(_STREAM_BOUNDARY));
+    }
+    if (fb) {
+      esp_camera_fb_return(fb);
+      fb = NULL;
+      _jpg_buf = NULL;
+    } else if (_jpg_buf) {
+      free(_jpg_buf);
+      _jpg_buf = NULL;
+    }
+    if (res != ESP_OK) {
+      break;
+    }
+  }
+  return res;
+}
 
-static void start_webserver(void) {
+httpd_uri_t uri_get = {.uri = "/",
+                       .method = HTTP_GET,
+                       .handler = stream_handler,
+                       .user_ctx = NULL};
+
+static esp_err_t start_webserver(void) {
+  esp_err_t res = ESP_OK;
+
   httpd_config_t config = HTTPD_DEFAULT_CONFIG();
   config.lru_purge_enable = true;
 
   ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
-  if (httpd_start(&server, &config) == ESP_OK) {
+  res = httpd_start(&server, &config);
+  if (res == ESP_OK) {
     ESP_LOGI(TAG, "Registering URI handlers");
     httpd_register_uri_handler(server, &uri_get);
   }
+
+  return res;
 }
 
 static void event_handler(void *arg, esp_event_base_t event_base,
@@ -178,9 +246,11 @@ static void init_wifi(const char *ssid, const char *ssid_pass,
 void app_main() {
   init_system();
   init_wifi("OliveiraTravi_2.4", "PinhosHouse2016", "micro-monitor");
+  ESP_ERROR_CHECK(app_camera_init());
 
   xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_BIT, pdFALSE, pdFALSE,
                       portMAX_DELAY);
 
-  start_webserver();
+  ESP_ERROR_CHECK(start_webserver());
+  ESP_LOGI(TAG, "!!!!!!!!!! Ready !!!!!!!!!!");
 }
